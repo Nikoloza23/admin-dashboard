f14b433edecd04527d961e8231f305ae4c00db89
nick <abashia.nikoloza@gmail.com>
2022-05-21 13:02:04 +0400

    admin-dashboard

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,8 @@
+import Chart from "../../components/chart/Chart";
+import Featured from "../../components/featured/Featured";
 import Navbar from "../../components/navbar/Navbar";
 import SideBar from "../../components/sidebar/SideBar";
+import Widget from "../../components/widget/Widget";
 
 import "./home.scss";
 
@@ -9,6 +12,16 @@ const Home = () => {
       <SideBar />
       <div className="homeContainer">
         <Navbar />
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
+        </div>
+        <div className="charts">
+          <Featured />
+          <Chart />
+        </div>
       </div>
     </div>
   );

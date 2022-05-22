d45e6fdc893e3f15f08f389f7ca2d32e8f5b7e72
nick <abashia.nikoloza@gmail.com>
2022-05-22 12:26:36 +0400

    admin-dashboard

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,9 @@
-import Chart from "../../components/chart/Chart";
-import Featured from "../../components/featured/Featured";
-import Navbar from "../../components/navbar/Navbar";
 import SideBar from "../../components/sidebar/SideBar";
+import Navbar from "../../components/navbar/Navbar";
 import Widget from "../../components/widget/Widget";
+import Featured from "../../components/featured/Featured";
+import Chart from "../../components/chart/Chart";
+import Table from "../../components/table/Table";
 
 import "./home.scss";
 
@@ -21,6 +22,10 @@ const Home = () => {
         <div className="charts">
           <Featured />
           <Chart />
+        </div>
+        <div className="listContainer">
+          <div className="listTitle">Latest Transactions</div>
+          <Table />
         </div>
       </div>
     </div>

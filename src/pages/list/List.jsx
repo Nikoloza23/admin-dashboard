bdb0e33f4e7549c0728b06614530b479898ff9f4
nick <abashia.nikoloza@gmail.com>
2022-05-22 14:33:35 +0400

    admin-dashboard

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,8 +1,17 @@
-import './list.scss'
+import Datatable from "../../components/datatable/Datatable";
+import Navbar from "../../components/navbar/Navbar";
+import SideBar from "../../components/sidebar/SideBar";
+import "./list.scss";
 const List = () => {
   return (
-    <div>List</div>
-  )
-}
+    <div className="list">
+      <SideBar />
+      <div className="listContainer">
+        <Navbar />
+        <Datatable />
+      </div>
+    </div>
+  );
+};
 
-export default List
+export default List;
